7a3a4bd0850a1212c2b9d55fd9647bdde5423de4
calipto722 <141640050+calipto722@users.noreply.github.com>
2024-02-18 19:32:23 -0300

    terminado 6

--- Ejercicio6.js
+++ Ejercicio6.js
@@ -4,3 +4,14 @@ Leer una cadena de texto desde un prompt y generar un array con la función spli
 Posteriormente, mostrar la siguiente información: Número de palabras, primera palabra,
 última palabra, las palabras colocadas en orden inverso, las palabras ordenadas de la “a”
 hasta la “z” y las palabras ordenadas de la “z” hasta la ”a”.*/
+
+var cadena = prompt("ingresar una cadena");
+var separador =' '
+var areglo_cadena = cadena.split(separador); 
+console.log("arreglo " + areglo_cadena)
+console.log('cantidad de palabras en el arreglo : ' + areglo_cadena.length);
+console.log(' primer palabra en el arreglo : ' + areglo_cadena[0]);
+console.log('ultima palabra del arreglo : ' + areglo_cadena[areglo_cadena.length - 1]);
+console.log(' Arreglo inverso ' + areglo_cadena.reverse());
+console.log('Arreglo ordenado de A-Z : ' + areglo_cadena.sort());
+console.log('Arreglo ordenado de Z-A : ' + areglo_cadena.sort().reverse());

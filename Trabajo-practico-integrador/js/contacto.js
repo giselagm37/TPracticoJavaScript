0b4d09cd2d797b5794033971b778e5b2d653bfd0
guido <guido-nunez@hotmail.com>
2024-03-05 20:57:11 -0300

    contacto v1

--- Trabajo-practico-integrador/js/contacto.js
+++ Trabajo-practico-integrador/js/contacto.js
@@ -1,12 +1,60 @@
-function validacion(){
+function validacion() {
     const nombre = document.getElementById("nombre");
-    const email = document.getElementById("e-mail");
-    const textarea = document.getElementById("textarea");
-    /* creamos Los patrones de validacion de los campos */
-    const patron = /^[a-zA-ZÁÉÍÓÚáéíóúÑñ\s]+$/;
-    const patronEmail = /^[a-zA-Z0-9_%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    const patronConsulta = /^[a-zA-Z0-9.-_?,!\s]+$/;
-    /* creamos un arreglo y Le cargamos Los valores de Los campos */
-    const datos = [apellido.value, nombre.value, email.value, textarea.value];
+    const email = document.getElementById("email");
+    const textarea = document.getElementById("mensaje");
+    const texErrorNombre = document.getElementById("texErrorNombre");
+    const texErrorEmail = document.getElementById("texErrorEmail");
+    const texErrorMensaje = document.getElementById("texErrorMensaje");
     
+    
+    const patronNombre = /^[a-zA-ZÁÉÍÓÚáéíóúÑñ\s]+$/;
+    const patronEmail = /^[a-zA-Z0-9_%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const patronMensaje = /^[a-zA-Z0-9.-_?,!\s]+$/;
+
+    
+    texErrorNombre.innerHTML = "";
+    texErrorEmail.innerHTML = "";
+    texErrorMensaje.innerHTML = "";
+
+    
+    if (!patronNombre.test(nombre.value)) {
+        texErrorNombre.innerHTML = "Nombre incorrecto";
+        return false;
     }
+
+    
+    if (!patronEmail.test(email.value)) {
+        texErrorEmail.innerHTML = "Email incorrecto";
+        return false;
+    }
+
+    
+    if (!patronMensaje.test(textarea.value)) {
+        texErrorMensaje.innerHTML = "Mensaje incorrecto";
+        return false;
+    }
+
+    mostrarDatosEnviados(nombre.value, email.value, textarea.value);
+    
+    return true;
+}
+
+function mostrarDatosEnviados(nombre, email, mensaje) {
+    const datosEnviadosDiv = document.createElement("div");
+
+    const nombreParrafo = document.createElement("p");
+    nombreParrafo.textContent = "Nombre: " + nombre;
+    datosEnviadosDiv.appendChild(nombreParrafo);
+
+    const emailParrafo = document.createElement("p");
+    emailParrafo.textContent = "Email: " + email;
+    datosEnviadosDiv.appendChild(emailParrafo);
+
+    const mensajeParrafo = document.createElement("p");
+    mensajeParrafo.textContent = "Mensaje: " + mensaje;
+    datosEnviadosDiv.appendChild(mensajeParrafo);
+
+    // Agrega los datos enviados al final del formulario
+    const contenedorFormulario = document.querySelector(".contenedor");
+    contenedorFormulario.appendChild(datosEnviadosDiv);
+}

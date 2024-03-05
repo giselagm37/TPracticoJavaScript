7867ca3223b8fb31fb10689032d07d5259b57d95
guido <guido-nunez@hotmail.com>
2024-03-05 18:34:38 -0300

    login usuario validacion

--- Trabajo-practico-integrador/js/login.js
+++ Trabajo-practico-integrador/js/login.js
@@ -1,17 +1,15 @@
-function validar(){
-var contrase = document.getElementById("contrasena").value;
-var user = document.getElementById("usuario").value;
-var texerroruser = document.getElementById("texerroruser");
-/* creamos Los patrones de validacion de los campos */
-const patron = /^[a-zA-ZÁÉÍÓÚáéíóúÑñ\s]+$/;
-/* creamos un arreglo y Le cargamos Los valores de Los campos */
-const datos = [usuario.value,contrase.value];
-console.log(`nnnnnnnnn`);
-if(!user.value ==`d` ){
-   texerroruser.innerHTML = "usuario incorrecto"
-   console.log(`ddddddd`);
-   return false;
-   
-}
-return true;
-}
+function validar() {
+   var contrase = document.getElementById("contrasena");
+   var user = document.getElementById("usuario");
+   var texerroruser = document.getElementById("texerroruser");
+ /* creamos Los patrones de validacion de los campos */
+ const patron = /^[a-zA-ZÁÉÍÓÚáéíóúÑñ\s]+$/;
+ /* creamos un arreglo y Le cargamos Los valores de Los campos */
+ const datos = [usuario.value,contrase.value];
+   if (user.value.toLowerCase() !== 'guido') {
+     texerroruser.innerHTML = "Usuario incorrecto";
+     return false;
+   }
+ 
+   return true;
+ }

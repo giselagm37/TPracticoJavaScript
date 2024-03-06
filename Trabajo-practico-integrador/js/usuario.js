c3fcfb2904df0d67a819a42e91433bf2485f3717
calipto722 <141640050+calipto722@users.noreply.github.com>
2024-03-06 19:29:23 -0300

    cccc

--- Trabajo-practico-integrador/js/usuario.js
+++ Trabajo-practico-integrador/js/usuario.js
@@ -1,23 +1,22 @@
 function agregarUsuario() {
   // Obtener los valores de los campos del formulario
-  var nombreu = document.getElementById("nombre").value;
-  var apellidou = document.getElementById("apellido").value;
-  var telefonou = document.getElementById("telefono").value;
-  var direccionu = document.getElementById("direccion").value;
-  var usuarioidu = document.getElementById("usuario").value;
-  var contrasenau = document.getElementById("contrasena").value;
-
+  var nombreu = document.getElementById("nombre").value.trim();
+  var apellidou = document.getElementById("apellido").value.trim();
+  var telefonou = document.getElementById("telefono").value.trim();
+  var direccionu = document.getElementById("direccion").value.trim();
+  var usuarioidu = document.getElementById("usuario").value.trim();
+  var contrasenau = document.getElementById("contrasena").value.trim();
 
   var textoerror = document.getElementById("texError");
- if (nombreu == " ") {
 
-  textoerror.innerHTML= "COMPLETE TODOS LOS CAMPOS";
-  setTimeout(function() {
-    textoerror.innerHTML = "";
-  }, 4000);
-}else{
-
-  let usuario = {
+  // Validar que los campos no estén vacíos
+  if (nombreu === "" || apellidou === "" || telefonou === "" || direccionu === "" || usuarioidu === "" || contrasenau === "") {
+    textoerror.innerHTML = "COMPLETE TODOS LOS CAMPOS";
+    setTimeout(function() {
+      textoerror.innerHTML = "";
+    }, 4000);
+  } else {
+    let usuario = {
       usuarioid: usuarioidu,
       contrasena: contrasenau,
       nombre: nombreu,
@@ -25,11 +24,13 @@ function agregarUsuario() {
       telefono: telefonou,
       direccion: direccionu,
       login: false
-  };
+    };
 
-  // Almacenar en localStorage
-  localStorage.setItem("user", JSON.stringify(usuario));
+    // Almacenar en localStorage
+    localStorage.setItem("user", JSON.stringify(usuario));
 
-  // Redireccionar a otra página
-  window.location.href = "login.html"; // Cambia "nueva_pagina.html" por la URL de la página a la que deseas redireccionar
-}}
+    // Redireccionar a otra página
+    window.location.href = "login.html"; // Cambia "nueva_pagina.html" por la URL de la página a la que deseas redireccionar
+  }
+}
+
